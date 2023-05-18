4bceac6e1b300d4440887115bd7e7c0ce1a5cd37
M@nojrpms <30459831+VV-MANOJ@users.noreply.github.com>
2023-05-18 19:50:45 +0530

    Update newinstall.sh

--- newinstall.sh
+++ newinstall.sh
@@ -7,11 +7,11 @@ packages=(
 )
 
 # Update the package manager and install the specified packages
-sudo yum update -y
+sudo apt update -y
 
 for package in "${packages[@]}"
 do
-    sudo yum install -y "$package"
+    sudo apt install -y "$package"
     $package --version
     if [ $? -eq 0 ]; then
       echo "$package install succeeded!"
@@ -20,9 +20,9 @@ do
     fi
 done
 
-sudo yum install -y yum-utils shadow-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+sudo apt install -y yum-utils shadow-utils
+sudo apt-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo apt -y install terraform
 terraform version
 if [ $? -eq 0 ]; then
       echo "terraform install succeeded!"
@@ -30,7 +30,7 @@ else
       echo "terraform install failed!"
 fi
 
-sudo yum  python3-pip python3-devel libffi-devel
+sudo apt  python3-pip python3-devel libffi-devel
 sudo pip3 install ansible==2.9.20
 ansible --version
 if [ $? -eq 0 ]; then
